c5d2c84cad41e6498a827b2be512f044792134dd
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-02 10:19:01 +0530

    Configuration to s3

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "4.65.0"
     }
   }
+   #s3 Configuration
+  #https://developer.hashicorp.com/terraform/language/settings/backends/s3
+  backend "s3" {
+    bucket = "timing-remote"
+    key    = "automated-vpc"
+    region = "us-east-1"
+    dynamodb_table = "timing-remote-dynamodb"
+  }
 }
 
 # provider "aws" {
