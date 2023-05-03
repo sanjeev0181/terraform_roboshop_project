8d2b2d8e0684c311e264080804b2ad4f1d4bef6d
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-03 10:10:34 +0530

    adding terraform in internet_cidr block .tfvars

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -11,6 +11,14 @@ variable "private_subnets_cidr" {
     type = string
 }
 
+variable "internet_cidr" {
+    type = string
+}
+
+variable "private_subnets_cidr" {
+    type = string
+}
+
 
 variable "tags" {
   type = map
