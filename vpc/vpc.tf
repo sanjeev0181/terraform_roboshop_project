e046dbe78efc4e0bdbc53cf7d6aeedf9f1920262
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-03 10:11:13 +0530

    merge in tags in vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -4,7 +4,9 @@ resource "aws_vpc" "main" {
   cidr_block       = var.cidr
   instance_tenancy = "default"
 
-  tags = var.tags
+  tags = merge(var.tags, {
+    Name = "timing"
+  } )
 }
 
 #subents
@@ -14,14 +16,18 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id  #if will fetch VPCID created form above code.
   cidr_block =  var.public_subnets_cidr #"10.0.1.0/24"
 
-  tags = var.tags
+  tags = merge(var.tags, {
+    Name = "public-subnets"
+  } )
 }
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id  #if will fetch VPCID created form above code.
   cidr_block = var.private_subnets_cidr #"10.0.2.0/24"
 
-  tags = var.tags
+  tags = merge(var.tags, {
+    Name = "private-subnets"
+  } )
 }
 
 
@@ -31,7 +37,9 @@ resource "aws_subnet" "private" {
 resource "aws_internet_gateway" "automated-igw" {
   vpc_id = aws_vpc.main.id  #internet gateway depends of VPC
 
-  tags = var.tags
+  tags = merge(var.tags, {
+    Name = "timing-igw"
+  } )
 }
 
 #Route Tables
@@ -41,22 +49,26 @@ resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block =  var.internet_cidr #"0.0.0.0/0"
     gateway_id = aws_internet_gateway.automated-igw.id
   }
 
-   tags = var.tags
+   tags = merge(var.tags, {
+    Name = "public_route_table"
+  } )
 }
 
 resource "aws_route_table" "private-rt" {  #for private route we don't attach IGW, we attach NAT
     vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.internet_cidr #"0.0.0.0/0"
     gateway_id = aws_nat_gateway.example.id
   }
 
-   tags = var.tags
+   tags = merge(var.tags, {
+    Name = "private-route-table"
+  } )
 }
 
 #Elastic ip
@@ -73,7 +85,9 @@ resource "aws_nat_gateway" "example" {
   allocation_id = aws_eip.auto_elb.id
   subnet_id     = aws_subnet.public_subnets_cidr.id #public ip
 
-  tags = var.tags
+  tags = merge(var.tags, {
+    Name = "timing-nat"
+  } )
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
@@ -85,10 +99,10 @@ resource "aws_nat_gateway" "example" {
 
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public_subnets_cidr.id
-  route_table_id = aws_route_table.public-rt.id
+  route_table_id = aws_route_table.public-route-table.id
 }
 
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private_subnets_cidr.id
-  route_table_id = aws_route_table.private-rt.id
+  route_table_id = aws_route_table.private-route-table.id
 }
