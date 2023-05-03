2f77c430ae0d44bef057f2142b1ebfca7df76ba7
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-03 09:46:36 +0530

    adding variables

---
+++ vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "cidr" {
+    type = string
+}
+
+variable "public_subnets_cidr" {
+  type = string
+
+}
+
+variable "private_subnets_cidr" {
+    type = string
+}
+
+
+variable "tags" {
+  type = map
+}
