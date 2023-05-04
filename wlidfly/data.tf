1881af992177088987f8eef5605d345f5a228cea
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 13:40:45 +0530

    adding

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -3,14 +3,15 @@ resource "aws_instance" "wlidfly_web"{
   ami = "ami-007855ac798b5175e" #AMI ID will be different every region, this is OS
   instance_type = "t3.medium"
   key_name = "ajith-keys"
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y git",
-      "sudo git clone https://github.com/sanjeev0181/terraform_roboshop_project.git",
-      "cd terraform_roboshop_project/wlidfly",
-      "bash wlidfly.sh",
-    ]
-  }
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo apt update -y",
+  #     "sudo apt install -y git",
+  #     "sudo git clone https://github.com/sanjeev0181/terraform_roboshop_project.git",
+  #     "cd terraform_roboshop_project/wlidfly",
+  #     "bash wlidfly.sh",
+  #   ]
+  # }
+  user_data = "${file(bash wlidfly.sh)}"
 }
 
