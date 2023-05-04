9c554335fac804e140b6b1367b2a08660cb6d52a
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 13:01:34 +0530

    adding

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "wlidfly_web"{
   ami = "ami-007855ac798b5175e" #AMI ID will be different every region, this is OS
   instance_type = "t3.medium"
   key_name = "ajith-key"
+  user_data = <<-EOF
   #!/bin/bash
   sudo apt update -y
   sudo apt -y install default-jdk
