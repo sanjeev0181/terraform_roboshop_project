d180e8850f0188d79c8d394045b19bfda74547c0
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 13:07:11 +0530

    adding

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "wlidfly_web"{
   ami = "ami-007855ac798b5175e" #AMI ID will be different every region, this is OS
   instance_type = "t3.medium"
-  key_name = "ajith-key"
+  key_name = "ajith-keys"
   user_data = <<-EOF
   #!/bin/bash
   sudo apt update -y
