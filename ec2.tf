552e3bf37fcfacfb51ebe3ac57f6de767c1576fd
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 11:27:28 +0530

    adding data.aws_ami.example.image_id

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web"{
 
-    ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
+    #ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
+    ami = data.aws_ami.example.image_id
     instance_type = "t3.micro"
     
 }
