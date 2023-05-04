64413cb951edc13a846a8ddddfab29cd43a0f36d
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 13:24:57 +0530

    adding

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "wlidfly_web"{
   ami = "ami-007855ac798b5175e" #AMI ID will be different every region, this is OS
   instance_type = "t3.medium"
   key_name = "ajith-keys"
-  user_data = "${file("wlidfly.sh")}"
+  user_data = "${file("bash wlidfly.sh")}"
 }
 
