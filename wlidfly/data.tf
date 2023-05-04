e9a688f339b75ea0d5b0140e8bc9d54e65fc8fa8
sanjeev0181 <adithyasanjeev01996@gmail.com>
2023-05-04 12:49:32 +0530

    adding

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -1,31 +1,6 @@
-data "aws_ami" "wliffly_ec2" {
-  most_recent      = true
-  owners           = ["521045274894"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
 
 resource "aws_instance" "wlidfly_web"{
-  #ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
-  ami = data.aws_ami.wliffly_ec2.image_id 
+  ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
   instance_type = "t3.medium"
 }
 
