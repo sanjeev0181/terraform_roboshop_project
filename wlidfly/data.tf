981682cb7082adf49b81568c26da5b0b872b8329
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-05 23:12:11 +0530

    adding

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -39,7 +39,8 @@
  
 # Launch instance with Windows Server AMI
 resource "aws_instance" "windows_instance" {
-  ami           = "ami-0dfe34254aac43e0c" # cloudimg-wildfly26-windows-server2022v1.0.1-d1f39bc6-6cc7-49d7-9202-b13778c25d0c
+  #ami           = "ami-0dfe34254aac43e0c" # cloudimg-wildfly26-windows-server2022v1.0.1-d1f39bc6-6cc7-49d7-9202-b13778c25d0c
+  ami  = "ami-0bde1eb2c18cb2abe"
   instance_type = "t2.micro"
   
   tags = {
@@ -52,7 +53,8 @@ resource "aws_instance" "windows_instance" {
 
 # Launch instance with Linux Server AMI
 resource "aws_instance" "linux_instance" {
-  ami           = "ami-0b7beba926e1f4233" # cloudimg-jboss-eap74-alma-linux8v1.0.0-88ddbac8-96ff-42cc-bf51-3fc704eaba4f
+  #ami           = "ami-0b7beba926e1f4233" # cloudimg-jboss-eap74-alma-linux8v1.0.0-88ddbac8-96ff-42cc-bf51-3fc704eaba4f
+  ami  = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
    
   tags = {
