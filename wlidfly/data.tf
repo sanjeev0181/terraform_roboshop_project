2876270c90a774352f2ef9363ed85efd410e97d1
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-05 09:46:29 +0530

    add

--- wlidfly/data.tf
+++ wlidfly/data.tf
@@ -3,15 +3,37 @@ resource "aws_instance" "wlidfly_web"{
   ami = "ami-007855ac798b5175e" #AMI ID will be different every region, this is OS
   instance_type = "t3.medium"
   key_name = "ajith-keys"
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y git",
-      "sudo git clone https://github.com/sanjeev0181/terraform_roboshop_project.git",
-      "cd terraform_roboshop_project/wlidfly/",
-      "bash wlidfly.sh",
-    ]
-  }
-  #user_data = "${file("./wlidfly.sh")}"
+  user_data = file("${path.module}/wlidfly.sh")
 }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt update -y",
+#       "sudo apt install -y git",
+#       "sudo git clone https://github.com/sanjeev0181/terraform_roboshop_project.git",
+#       "cd terraform_roboshop_project/wlidfly/",
+#       "bash wlidfly.sh",
+#     ]
+#   }
+#   user_data = <<EOF
+#   #!/bin/bash
+#     sudo apt update -y
+#     sudo apt -y install default-jdk
+#     sudo apt install curl wget -y 
+#     wget https://github.com/wildfly/wildfly/releases/download/26.0.0.Final/wildfly-26.0.0.Final.tar.gz
+#     tar -xf wildfly-*.Final.tar.gz
+#     sudo mv wildfly-*Final /opt/wildlfy
+#     sudo groupadd --system wildfly
+#     sudo useradd -s /sbin/nologin --system -d /opt/wildfly  -g wildfly wildfly
+#     sudo mkdir /etc/wildfly
+#     sudo cp /opt/wildfly/docs/contrib/scripts/systemd/wildfly.conf /etc/wildfly/
+#     sudo cp /opt/wildfly/docs/contrib/scripts/systemd/wildfly.service /etc/systemd/system/
+#     sudo cp /opt/wildfly/docs/contrib/scripts/systemd/launch.sh /opt/wildfly/bin/
+#     sudo chmod +x /opt/wildfly/bin/launch.sh
+#     sudo chown -R wildfly:wildfly /opt/wildfly
+#     sudo systemctl daemon-reload
+#     sudo systemctl start wildfly
+#     sudo systemctl enable wildfly
+#     sudo systemctl status wildfly
+#  EOF
 
+ 
