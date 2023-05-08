39d45e9de61bada11c59c3b5faa3b3d5b5aefdcf
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-08 10:12:53 +0530

    adding to count

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,37 @@ resource "aws_instance" "web"{
         Name = var.instance[count.index] #index always  start from 0.if start
     }   
 }
+
+
+# condition is if variable of key is defined then use that, other wise use dafult key
+
+
+# resource "aws_instance" "web-1"{
+#     ami = "ami-022d03f649d12a49d" #AMI ID will be different every region, this is OS
+#     instance_type = "t3.micro"
+#     count = 0 #if count equal to zero terraform will not create the resource
+# }
+
+# resource "aws_instance" "web-1"{
+#     ami = "ami-022d03f649d12a49d" #AMI ID will be different every region, this is OS
+#     instance_type = var.is_test ? "t3.micro" : "t3.large"
+# }
+
+# resource "aws_instance" "server"{
+#     for_each = var.servers #here you need to give a map to iterate
+#     ami = "ami-022d03f649d12a49d" #AMI ID will be different every region, this is OS
+#     instance_type = each.value
+#     tags = {
+#       Name = each.key
+#     }
+# }
+
+# }
+
+resource "aws_instance" "server"{
+    ami = "ami-022d03f649d12a49d" #AMI ID will be different every region, this is OS
+    instance_type = "t3.micro"
+    tags = {
+      Name = local.instance_name
+    }
+}
