e099e85a524f687f7127b842d308afbe2ce29153
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-08 09:39:37 +0530

    adding to count

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,10 @@ resource "aws_instance" "web"{
     #ami = "ami-046b3d63c4fe1b3cf" #AMI ID will be different every region, this is OS
     ami = "ami-0dfe34254aac43e0c"
     instance_type = "t3.micro"
-    count = 3  #our requirement is to create 
+    #count = 3  #our requirement is to create 
+    count = lenght(var.instances)
     tags = {
         #Name = "WEB-${count.index}"
-        Name = var.instance[count.index]
+        Name = var.instance[count.index] #index always  start from 0.if start
     }   
 }
