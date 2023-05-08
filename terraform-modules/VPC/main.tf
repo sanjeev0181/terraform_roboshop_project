a21a5c969c15e419fe66d0d6a765e970da0f266d
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-08 18:54:48 +0530

    adding the main.tf

--- terraform-modules/VPC/main.tf
+++ terraform-modules/VPC/main.tf
@@ -1,5 +1,5 @@
 #This will create vpc
 resource "aws_vpc" "main" {
-  cidr_block = "195.0.0.0/16"
+  cidr_block = var.cidr
   instance_tenancy =  "default"
 }
---
+++ terraform-modules/VPC/variables.tf
@@ -0,0 +1,4 @@
+variable "cidr" {
+  type = string
+  default = "195.0.0.0/16"
+}
