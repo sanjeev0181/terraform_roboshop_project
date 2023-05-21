14c4cd45922fe39155d987a31fe3dabb01f17f44
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-21 22:52:57 +0530

    adding eip and nat gateway in main.tf and varaiables.tf in files

--- terraform-modules/vpc/main.tf
+++ terraform-modules/vpc/main.tf
@@ -59,7 +59,7 @@ resource "aws_eip" "eip" {
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public.id
-  tags = var. 
+  tags = var.nat_tags
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
