913c23b863e5a34a3eb2f89a66bc1dc4da494caa
sanjeev0181 <adithyasanjeev1996@gmail.com>
2023-05-21 23:33:13 +0530

    adding output  in main.tf and varaiables.tf in files

--- terraform/session06/main.tf
+++ terraform/session06/main.tf
@@ -10,3 +10,32 @@ module "vpc" {
   private_rt_tags = var.private_rt_tags
   db_subnets_tags = var.db_subnets_tags
 }
+
+#As a user you want to create security group
+
+resource "aws_security_group" "web" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = 0.0.0.0/0
+  
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "timing-web"
+  }
+}
